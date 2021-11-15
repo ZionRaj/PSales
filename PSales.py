c4c21a6385eecc4c027b0af171e50d2fb10c0394
ZionRaj <94182158+ZionRaj@users.noreply.github.com>
2021-11-15 17:02:05 +0530

    Update PSales.py

--- PSales.py
+++ PSales.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from tkinter import *
-from functools import partial
+#from tkinter import *
+#from functools import partial
 
 
 """
@@ -8,29 +8,9 @@ from functools import partial
 
 """
 
-def validateLogin(username, password):
-	print("username entered :", username.get())
-	print("password entered :", password.get())
-	return
+st.title('PartnerSales Collection Hub')
 
-#window
-tkWindow = Tk()  
-tkWindow.geometry('400x150')  
-tkWindow.title('PartnerSales Login')
+Username = st.text_input('Username', placeholder='TEN\\UXXXXXXX')
+Password = st.text_input('Password')
 
-#username label and text entry box
-usernameLabel = Label(tkWindow, text="User Name").grid(row=0, column=0)
-username = StringVar()
-usernameEntry = Entry(tkWindow, textvariable=username).grid(row=0, column=1)  
-
-#password label and password entry box
-passwordLabel = Label(tkWindow,text="Password").grid(row=1, column=0)  
-password = StringVar()
-passwordEntry = Entry(tkWindow, textvariable=password, show='*').grid(row=1, column=1)  
-
-validateLogin = partial(validateLogin, username, password)
-
-#login button
-loginButton = Button(tkWindow, text="Login", command=validateLogin).grid(row=4, column=0)  
-
-tkWindow.mainloop()
+st.button('Login')
