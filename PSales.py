b83eb7e6f5ebe71c5db816e3b7d294c122321bb6
ZionRaj <94182158+ZionRaj@users.noreply.github.com>
2021-11-15 19:42:12 +0530

    Update PSales.py

--- PSales.py
+++ PSales.py
@@ -9,6 +9,7 @@ import streamlit as st
 """
 
 #st.title('PartnerSales Collection Hub')
+
 st.write('Login Page')
 
 Uname = st.text_input('Username')
@@ -19,10 +20,14 @@ Pwd = st.text_input('Password')
 if st.button('Login'):
 	if len(Uname) >= 1 or len(Pwd) >= 1:
 		st.write('Logging in...')
+		st.set_page_config(
+			page_title = 'PartnerSales report upload',
+			page_icon = st.image(load_image_from_local("C:\\Users\\u6075712\\OneDrive - Thomson Reuters Incorporated\\Documents\\zion\\ReutersLogo.png"), width=300),
+			layout="wide",
+        	initial_sidebar_state="expanded")
 	else:
 		st.write('Username or Password cannot be empty')
 
 #if len(Uname) > 1 or len(Pwd) > 1:
 	#st.write ('Logging in...')
-
 
